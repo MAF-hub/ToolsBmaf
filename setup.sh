f4ed47bcafab499d191a38ef18a6eac8dbeb5a95
MAF-hub <68975444+MAF-hub@users.noreply.github.com>
2020-10-11 13:08:54 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+apt install toilet
+apt install python3
+pip3 install -r requirements.txt
+clear
+toilet -f pagga END.. --filter gay --filter border|lolcat
+setterm -foreground red
+echo ''
+echo 'gracias por instalar mi script'
+echo ''
+echo 'Para iniciar el programa escriba python3 binning.py'
+echo ''
+setterm -foreground white
