e980865ebd9bee3cbdecc06ec54bbe9f19a03d82
MAF-hub <68975444+MAF-hub@users.noreply.github.com>
2020-10-13 14:18:41 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ apt install python
 pip3 install  bs4
 pip3 install requests
 clear
+touch code.txt
+touch db_bins.txt
 toilet -f pagga END.. --filter gay --filter border|lolcat
 setterm -foreground red
 echo ''
